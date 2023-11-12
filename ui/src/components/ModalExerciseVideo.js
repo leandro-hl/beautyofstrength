b12e59b967e8e7e27c2d04efacd1fbd21799e943
Leandro Herenu <nleandro.hl@gmail.com>
2023-11-11 23:54:54 -0300

    beta0.23-fix

--- ui/src/components/ModalExerciseVideo.js
+++ ui/src/components/ModalExerciseVideo.js
@@ -5,12 +5,36 @@ import {AppContext, setData} from "../context";
 class ModalExerciseVideo extends Component {
     static contextType = AppContext
 
+    constructor(props) {
+        super(props);
+        this.calculateDimensions()
+    }
+
+    calculateDimensions() {
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+        const aspectRatio = 16 / 9;
+        // Calculate width and height based on the screen size and aspect ratio
+        let videoWidth = screenWidth; // Start with the full width
+        let videoHeight = videoWidth * aspectRatio;
+
+        // Check if the calculated height is greater than the screen height
+        if (videoHeight > screenHeight) {
+            // If yes, recalculate both based on the screen height
+            videoHeight = screenHeight;
+            videoWidth = videoHeight / aspectRatio;
+        }
+
+        this.state = { width: videoWidth, height: videoHeight };
+    }
+
     handleClose = () => {
         this.context.dispatch(setData({videoCode: null}))
     }
 
     render() {
         const {state: {videoCode}} = this.context
+        const {width, height} = this.state
         return (
             <Modal
                 className={'embeded-video no-padding'}
@@ -21,8 +45,8 @@ class ModalExerciseVideo extends Component {
                 dimmer={'blurring'}
                 open={!!videoCode}
                 onClose={() => this.handleClose()}>
-                <Modal.Content className={'no-padding'} style={{height: 545}}>
-                    <iframe style={{border: 'unset'}} width="100%" height="545"
+                <Modal.Content className={'no-padding'} style={{height: height}}>
+                    <iframe style={{border: 'unset'}} width="100%" height={height}
                             src={"https://www.youtube.com/embed/"+videoCode}
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen>
