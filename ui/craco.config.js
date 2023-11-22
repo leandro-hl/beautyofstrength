3753f2583f6c02ab0a13b3d9bbfca0be825ec40f
Leandro Herenu <nleandro.hl@gmail.com>
2023-11-21 23:12:43 -0300

    beta0.29-fix

--- ui/craco.config.js
+++ ui/craco.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     plugins: [{ plugin: require('@semantic-ui-react/craco-less') }],
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            webpackConfig.mode = 'production';
+            webpackConfig.devtool = false;
+
+            return webpackConfig;
+        },
+    },
 }
