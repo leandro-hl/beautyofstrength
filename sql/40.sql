6a7620e5964024a7757e1cc32aec53bc281ee4e2
Leandro Herenu <nleandro.hl@gmail.com>
2024-01-13 12:30:34 -0300

    beta0.40-fix

--- sql/40.sql
+++ sql/40.sql
@@ -13,6 +13,7 @@ alter table useraccount add column code varchar(15) null;
 
 update useraccount set code='juan.solanilla' where username='juansolanilla@live.com';
 update useraccount set code='sergio.suares' where username='segiosuaress9@gmail.com';
+update useraccount set code='delparque.fit' where username='leandronherenu@gmail.com';
 update useraccount set code='guille.panak' where username='guillermopanak@gmail.com';
 
 
