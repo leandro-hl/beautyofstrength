cf5916f2f1a251f73126dffa580b943f9fe12789
Leandro Herenu <nleandro.hl@gmail.com>
2024-04-16 23:43:07 -0300

    fix spacings

--- ui/src/components/PageRoutineDetail.js
+++ ui/src/components/PageRoutineDetail.js
@@ -1430,23 +1430,23 @@ class PageRoutineDetail extends Component{
                                     routineStarted
                                 )))}
                             </div>
-                            <Divider horizontal>
-                                {
-                                    (!editionMode && !routineStarted && actionable) ?
+                            {
+                                (!editionMode && !routineStarted && actionable) ?
+                                    <Divider horizontal>
                                         !editRoutine ?
-                                            <PopUpDisabledAction
-                                                trigger={<Icon name={'plus'} className={'disabled-btn'}/>}/>
-                                            :
-                                            canEdit ?
-                                            <Icon name={'plus'} onClick={() => this.addWorkToGrouper(bg, j)}/> :
-                                                <Icon name={'plus'} onClick={() => this.addWorkToGrouper(bg, j)}/>
-                                            // <PopUpDisabledAction
-                                            //     disableHeader={'No es posible agregar'}
-                                            //     disableDescription={'Tu o un atleta ya marcaron esta rutina como completada u omitida'}
-                                            //     trigger={<Icon name={'plus'} className={'disabled-btn'}/>}/>
-                                        : null
-                                }
-                            </Divider>
+                                        <PopUpDisabledAction
+                                            trigger={<Icon name={'plus'} className={'disabled-btn'}/>}/>
+                                        :
+                                        canEdit ?
+                                        <Icon name={'plus'} onClick={() => this.addWorkToGrouper(bg, j)}/> :
+                                        <Icon name={'plus'} onClick={() => this.addWorkToGrouper(bg, j)}/>
+                                        {/*// <PopUpDisabledAction*/}
+                                        {/*//     disableHeader={'No es posible agregar'}*/}
+                                        {/*//     disableDescription={'Tu o un atleta ya marcaron esta rutina como completada u omitida'}*/}
+                                        {/*//     trigger={<Icon name={'plus'} className={'disabled-btn'}/>}/>*/}
+                                    </Divider>
+                                    : null
+                            }
                         </Segment>
                     ))}
                 </Accordion>
