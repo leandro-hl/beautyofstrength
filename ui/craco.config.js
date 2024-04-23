e79ec17bcc7c5e9dd336c4c1a692458350f4e313
Leandro Herenu <nleandro.hl@gmail.com>
2024-04-23 20:51:53 -0300

    - avoid webpack three

--- ui/craco.config.js
+++ ui/craco.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     plugins: [{ plugin: require('@semantic-ui-react/craco-less') }],
-    // webpack: {
-    //     configure: (webpackConfig, { env, paths }) => {
-    //         webpackConfig.mode = 'production';
-    //         webpackConfig.devtool = false;
-    //
-    //         return webpackConfig;
-    //     },
-    // },
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            webpackConfig.mode = 'production';
+            webpackConfig.devtool = false;
+
+            return webpackConfig;
+        },
+    },
 }
